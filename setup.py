d8c4223c0125ae1c1315574790deab827f87f702
Alexandre Decan <alexandre.decan@lexpage.net>
2018-04-03 18:23:53 +0200

    Fix package name & Travis

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ with open(path.join(here, 'README.md'), encoding='utf-8') as f:
     long_description = f.read()
 
 setup(
-    name='intervals',
+    name='python-intervals',
 
     # Versions should comply with PEP440.  For a discussion on single-sourcing
     # the version across setup.py and the project code, see
