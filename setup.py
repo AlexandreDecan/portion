32d0f4c3d7ba8d30f432d38fae924184b90a98cd
Alexandre Decan <alexandre.decan@lexpage.net>
2018-04-11 21:37:17 +0200

    Remove Python 3.7 (dev)

--- setup.py
+++ setup.py
@@ -36,7 +36,6 @@ setup(
         'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
-        'Programming Language :: Python :: 3.7',
     ],
     keywords='interval arithmetic range math',
 
