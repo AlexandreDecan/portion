0144698badf96174bf7dc57875a1061b6ac4c654
Alexandre Decan <alexandre.decan@lexpage.net>
2020-03-15 10:14:18 +0100

    Prepare 2.0.1

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,6 +1,7 @@
 # Changelog
 
-## Development
+
+## 2.0.1 (2020-03-15)
 
 ### Fixed
 
--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open(path.join(path.abspath(path.dirname(__file__)), 'README.md'), encoding
 
 setup(
     name='portion',
-    version='2.0.0',
+    version='2.0.1',
     license='LGPLv3',
 
     author='Alexandre Decan',
