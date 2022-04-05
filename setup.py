ec9189652a000fcf3db3a9db127a8e379e290fc9
Alexandre Decan <alexandre.decan@lexpage.net>
2022-04-05 14:28:31 +0200

    Support Python 3.10

--- setup.py
+++ setup.py
@@ -30,6 +30,7 @@ setup(
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
     ],
     keywords="interval operation range math",
     packages=find_packages(include=["portion"]),
