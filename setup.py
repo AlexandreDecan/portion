b944c8b0b05368f70b94ce3f15979fdf8dc9d320
Alexandre Decan <alexandre.decan@lexpage.net>
2023-10-14 12:33:03 +0000

    Support & tests for Python 3.12

--- setup.py
+++ setup.py
@@ -31,6 +31,7 @@ setup(
         "Programming Language :: Python :: 3.9",
         "Programming Language :: Python :: 3.10",
         "Programming Language :: Python :: 3.11",
+        "Programming Language :: Python :: 3.12",
     ],
     keywords="interval operation range math",
     packages=find_packages(include=["portion"]),
